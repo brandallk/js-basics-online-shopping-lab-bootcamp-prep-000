9fd3f96d0f515d68d831af7960817d007997187c
Benjamen King <brk.3rdof3kings@gmail.com>
2017-12-14 06:21:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,7 +78,10 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (typeof cardNumber === 'undefined') {
-    console.log()
+    console.log("Sorry, we don't have a credit card on file for you.");
+  } else {
+    var totalCost = total();
+    console.log(``)
   }
 }
 
