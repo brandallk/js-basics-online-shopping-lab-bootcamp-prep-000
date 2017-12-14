7c1f9e28151e0cbe1cf46e51bda207a92846e32f
Benjamen King <brk.3rdof3kings@gmail.com>
2017-12-14 06:22:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -81,7 +81,8 @@ function placeOrder(cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.");
   } else {
     var totalCost = total();
-    console.log(``)
+    console.log(`Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`);
+    
   }
 }
 
