1635277b674a4ab65ca0a8e0f89cffc92f1adac0
Benjamen King <brk.3rdof3kings@gmail.com>
2017-12-14 05:44:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,6 +55,31 @@ function viewCart() {
   }
 }
 
+function viewCart() {
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.");
+  } else {
+    var tally = "In your cart, you have ";
+    var lastIndex = cart.length - 1;
+    for (let itemIndex = 0; itemIndex < cart.length; itemIndex++) {
+      var itemName = Object.keys(cart[itemIndex])[0];
+      var price = cart[itemIndex][itemName];
+      if (itemIndex === lastIndex) {
+        tally += `${itemName} at $${price}.`;
+      } else if (itemIndex === lastIndex - 1) {
+        if (cart.length === 2) {
+          tally += `${itemName} at $${price} and `;
+        } else {
+          tally += `${itemName} at $${price}, and `;
+        }
+      } else {
+        tally += `${itemName} at $${price}, `;
+      }
+    }
+    console.log(tally);
+  }
+}
+
 function total() {
   return restructuredCart(cart).reduce( function(acc, item) {
     return acc + item.price; 
